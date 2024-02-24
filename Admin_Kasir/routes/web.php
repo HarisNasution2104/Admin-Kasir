e311ed4d0312d03604db76bf072a012524109df2
Nasution <120891496+HarisNasution2104@users.noreply.github.com>
2024-02-24 03:23:43 +0000

    install jetstream

--- Admin_Kasir/routes/web.php
+++ Admin_Kasir/routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn() => redirect()->route('login'));
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('home');
+    })->name('dashboard');
+});
+    Route::group(['middleware' => 'auth'], function(){
+    Route::get('/kategori/data',[KategoriControler::class,'data']);
+    Route::resource('/kategori',KategoriControler::class);
 });
